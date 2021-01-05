431456e85eb875534f9187ad7b21a32b98a19c34
dindin28 <Shtrikker28@gmail.com>
2021-01-05 02:56:41 +0200

    v 2.0
    At least:
    1) check spawn food
    2)check movement backward

--- main.cpp
+++ main.cpp
@@ -11,12 +11,23 @@ struct node {
     node* prev = NULL;
 };
 
+node* head;
+
 void clearGF(char *GF, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             *(GF + (i * size) + j) = ' ';
         }
     }
+}
+
+int calcSizeOfSnake(node* head) {
+    int counter = 0;
+    while (head) {
+        counter++;
+        head = head->next;
+    }
+    return counter;
 }
 
 void showGF(char *GF, int size) {
@@ -30,31 +41,66 @@ void showGF(char *GF, int size) {
         cout << "|\n";
     }
     for (int i = 0; i < size + 2; i++) cout << "-";
-    cout << endl;
+    cout << endl << "Score: " << calcSizeOfSnake(head) << endl;
 }
 
-// make prev pointer
 node* createSnake(char *GF, int size) {
     node* head = new node;
-    head->x = size / 2;
-    head->y = size / 2;
-    node* iteratorBuffer = head;
-    node* iterator;
-    int sizeOfSnake = 5;
-    for (int i = 1; i < sizeOfSnake; i++) {
-        iterator = new node;
-        iterator->x = head->x;
-        iterator->y = size / 2 + i;
-        iteratorBuffer->next = iterator;
-        iteratorBuffer = iterator;
+    const int sizeOfSnake = 2;
+    node* temp;
+    for (int i = 0; i < sizeOfSnake - 1; i++) {
+        temp = new node;
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
     }
-    iterator = head;
-    while (iterator) {
-        *(GF + size * iterator->y + iterator->x) = 'o';
-        iterator = iterator->next;
+    temp = head;
+    for (int i = 0; i < sizeOfSnake; i++) {
+        temp->x = size / 2;
+        temp->y = size / 2 + i;
+        *(GF + temp->y * size + temp->x) = 'o';
+        temp = temp->next;
     }
-    *(GF + size * head->y + head->x) = 'O';
+
+    //Check_Snake
+    /*temp = head;
+    cout << endl;
+    while (temp) {
+        cout << temp << ": " << temp->x << " " << temp->y << " " << temp->next << " " << temp->prev << " " << endl;
+        temp = temp->next;
+    }*/
+
+    *(GF + head->y * size + head->x) = 'O';
     return head;
+}
+
+void spawnFood(char* GF, int size) {
+    char* temp;
+    int counter = 0;
+    for (temp = GF; temp != GF + size * size; temp++) {
+        if (*temp != 'o' && *temp != 'O')
+            counter++;
+    }
+    int* freeCoord = new int[2 * counter];
+    int* tempCoord = freeCoord;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (*(GF + size * i + j) != 'o' && *(GF + size * i + j) != 'O') {
+                *tempCoord = j; *(tempCoord + 1) = i;
+                tempCoord += 2;
+            }
+        }
+    }
+
+    /*for (tempCoord = freeCoord; tempCoord != freeCoord + 2 * counter;) {
+        cout << *tempCoord << " " << *(tempCoord + 1) << endl;
+        tempCoord += 2;
+    }
+    cout << counter;*/
+
+    int buffer = rand() % counter;
+    //cout << *(freeCoord + buffer) << " " << *(freeCoord + buffer + 1) << endl;
+    *(GF + size * (*(freeCoord + buffer)) + *(freeCoord + buffer + 1)) = 'X';
 }
 
 int checkPressedButton(){
@@ -76,9 +122,18 @@ int checkPressedButton(){
     }else return 0;
 }
 
+int checkBorders(char* GF, int size, int x, int y) {
+    if (x >= size || x < 0 || y >= size || y < 0 || *(GF + size * y + x) == 'o') {
+        return 0;
+    }
+    else if (*(GF + size * y + x) == 'X') {
+        return 2;
+    }
+    else return 1;
+}
+
 node* moveSnake(char* GF, int size, node *head, int movement) {
     int x = head->x, y = head->y;
-    *(GF + head->y * size + head->x) = 'o';
     switch (movement) {
         case(1): {
             y--;
@@ -97,34 +152,70 @@ node* moveSnake(char* GF, int size, node *head, int movement) {
             break;
         }
     }
-    node* iterator = head;
-    while (iterator->next) iterator = iterator->next;
-    *(GF + iterator->y * size + iterator->x) = ' ';
-    iterator->x = x; iterator->y = y;
-    
+    switch (checkBorders(GF, size, x, y)) {
+        case(0): {
+            showGF(GF, size);
+            cout << "Game over" << endl;
+            system("pause");
+            exit(0);
+        }
+        case(1): {
+            *(GF + head->y * size + head->x) = 'o';
+            node* temp = head;
+            while (temp->next)
+                temp = temp->next;
+            temp->prev->next = NULL;
+            *(GF + temp->y * size + temp->x) = ' ';
+            temp->x = x; temp->y = y;
+            *(GF + temp->y * size + temp->x) = 'O';
+            head->prev = temp;
+            temp->next = head; temp->prev = NULL;
+            return temp;
+        }
+        case(2): {
+            *(GF + head->y * size + head->x) = 'o';
+            node* temp = new node;
+            temp->next = head;
+            head->prev = temp;
+            temp->x = x; temp->y = y;
+            *(GF + temp->y * size + temp->x) = 'O';
+            spawnFood(GF, size);
+            return temp;
+        }
+    }
 }
 
 int main()
 {
+    srand(time(NULL));
     clock_t start_t;
     system("title Snake");
-    const int size = 30;
+    const int size = 10;
     char GF[size][size];
     clearGF(&GF[0][0], size);
-    node* head = createSnake(&GF[0][0], size);
+    head = createSnake(&GF[0][0], size);
+    spawnFood(&GF[0][0], size);
     showGF(&GF[0][0], size);
     int movement = 1;
     int buffer = checkPressedButton();
-    if (buffer) movement = buffer;
+    int delay = 1000;
+    if (buffer)
+        movement = buffer;
     start_t = clock();
     while (1) {
-        if (clock() - start_t > 2000) {
+        if (clock() - start_t > delay) {
             system("cls");
-
+            head = moveSnake(&GF[0][0], size, head, movement);
+            showGF(&GF[0][0], size);
             start_t = clock();
         }
+
         buffer = checkPressedButton();
-        if (buffer) movement = buffer;
+        if (buffer)
+            movement = buffer;
+        if (calcSizeOfSnake(head) < 16) {
+            delay = 1000 - calcSizeOfSnake(head) * 50;
+        }
     }
     _getch();
 }
